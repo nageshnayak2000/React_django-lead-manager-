aa567de37df8dcf622a504525ed5e5f56f104653
[nageshnayak2000] <nageshnayak2050@gmail.com>
2020-03-25 22:50:01 +0530

    1st commit

--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -3,6 +3,6 @@ from rest_framework import viewsets, permissions
 from .serializers import LeadSerializer
 
 class LeadViewSet(viewsets.ModelViewSet):
-    queryset = Lead.objects.all()
+
     permission_classes = [permissions.AllowAny]
     serializer_class = LeadSerializer
