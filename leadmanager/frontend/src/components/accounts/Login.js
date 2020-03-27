1e3d71f23bb55421593e539c3ab83f11fe003a36
[nageshnayak2000] <nageshnayak2050@gmail.com>
2020-03-27 13:37:40 +0530

    4th commit

--- leadmanager/frontend/src/components/accounts/Login.js
+++ leadmanager/frontend/src/components/accounts/Login.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { login } from "../../actions/auth";
 
 export class Login extends Component {
   state = {
@@ -7,19 +10,27 @@ export class Login extends Component {
     password: ""
   };
 
+  static propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+  };
+
   onSubmit = e => {
     e.preventDefault();
-    console.log("Submit");
+    this.props.login(this.state.username, this.state.password);
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   render() {
+    if (this.props.isAuthenticated) {
+      return <Redirect to="/" />;
+    }
     const { username, password } = this.state;
     return (
       <div className="col-md-6 m-auto">
         <div className="card card-body mt-5">
-          <h2 className="text-center">Register</h2>
+          <h2 className="text-center">Login</h2>
           <form onSubmit={this.onSubmit}>
             <div className="form-group">
               <label>Username</label>
@@ -31,6 +42,7 @@ export class Login extends Component {
                 value={username}
               />
             </div>
+
             <div className="form-group">
               <label>Password</label>
               <input
@@ -41,6 +53,7 @@ export class Login extends Component {
                 value={password}
               />
             </div>
+
             <div className="form-group">
               <button type="submit" className="btn btn-primary">
                 Login
@@ -56,4 +69,8 @@ export class Login extends Component {
   }
 }
 
-export default Login;
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { login })(Login);
--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -4,32 +4,34 @@ import { connect } from "react-redux";
 import propTypes from "prop-types";
 
 export class Alerts extends Component {
-    static propTypes = {
-        error: propTypes.object.isRequired,
-        message: propTypes.object.isRequired
-    };
-    componentDidUpdate(prevProps) {
-        const { error, alert, message } = this.props;
-        if (error !== prevProps.error) {
-            if (error.msg.name) alert.error(`Name: ${error.msg.name.join()}`);
-            if (error.msg.email)
-                alert.error(`Email: ${error.msg.email.join()}`);
-            if (error.msg.message)
-                alert.error(`Message: ${error.msg.email.join()}`);
-        }
-        if (message !== prevProps.message) {
-            if (message.deleteLead) alert.success(message.deleteLead);
-            if (message.addLead) alert.success(message.addLead);
-        }
+  static propTypes = {
+    error: propTypes.object.isRequired,
+    message: propTypes.object.isRequired
+  };
+  componentDidUpdate(prevProps) {
+    const { error, alert, message } = this.props;
+    if (error !== prevProps.error) {
+      if (error.msg.name) alert.error(`Name: ${error.msg.name.join()}`);
+      if (error.msg.email) alert.error(`Email: ${error.msg.email.join()}`);
+      if (error.msg.message) alert.error(`Message: ${error.msg.email.join()}`);
+      if (error.msg.non_field_errors)
+        alert.error(error.msg.non_field_errors.join());
+      if (error.msg.username) alert.error(error.msg.username.join());
     }
-    render() {
-        return <Fragment />;
+    if (message !== prevProps.message) {
+      if (message.deleteLead) alert.success(message.deleteLead);
+      if (message.addLead) alert.success(message.addLead);
+      if (message.passwordNotMatch) alert.error(message.passwordNotMatch);
     }
+  }
+  render() {
+    return <Fragment />;
+  }
 }
 
 const mapStateToProps = state => ({
-    error: state.errors,
-    message: state.messages
+  error: state.errors,
+  message: state.messages
 });
 
 export default connect(mapStateToProps)(withAlert()(Alerts));
